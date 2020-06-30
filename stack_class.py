f6ca3fef75209b854febf13745ddfd1cd93a963f
Vitor Santos <vitorpaulasantos@gmail.com>
2020-06-29 22:54:41 -0300

    manipulando a pilha com os metodos da class

--- stack_class.py
+++ stack_class.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import List, Any
 
 class Stack:
@@ -16,3 +17,33 @@ class Stack:
             return []
         
         return self.__data[-1]
+
+    def repr(self) -> str:
+        return f'{self.__class__.__name__}({self.__data})'
+
+    def __iter__(self) -> Stack:
+        self.__index = len(self.__data)
+        return self
+
+    def __next__(self) -> Any:
+        if self.__index == 0:
+            raise StopIteration
+
+        self.__index -= 1
+        return self.__data[self.__index]
+
+    def __bool__(self) -> bool:
+        return bool(self.__data)
+
+if __name__ == "__main__":
+    stack = Stack()
+
+    stack.push('A')
+    stack.push('B')
+    stack.push('C')
+
+    for item in stack:
+        print(item)
+
+    while stack:
+        print(stack.pop())
