00fcc95025246fe66ea96ebf9dd08aedc0d7ac92
Vitor Santos <vitorpaulasantos@gmail.com>
2020-06-29 22:35:42 -0300

    add métodos Push e Pop na class

--- stack_class.py
+++ stack_class.py
@@ -1,5 +1,12 @@
 from typing import List, Any
 
 class Stack:
-    def __init__(self):
+    def __init__(self) -> None:
         self.__data: List[Any] = []
+        self.__index = 0
+
+    def push(self, item: Any) -> None:
+        self.__data.append(item)
+
+    def pop(self) -> Any:
+        return self.__data.pop()
