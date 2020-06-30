776d0a702766db22743ede7abb31bf9448018a32
Vitor Santos <vitorpaulasantos@gmail.com>
2020-06-29 22:10:15 -0300

    add comentario

--- pilha.py
+++ pilha.py
@@ -6,6 +6,7 @@ stack.append('A')
 stack.append('B')
 stack.append('C')
 
+# removendo item da pilha com os métodos da lista
 top_item = stack.pop() # Removendo C
 top_item = stack.pop() # removendo B
 
