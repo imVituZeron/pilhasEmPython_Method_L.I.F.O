588524a67f356a846bf2cd1b0228af77c2053649
Vitor Santos <vitorpaulasantos@gmail.com>
2020-06-29 22:06:38 -0300

    removendo item da pilha com métodos de uma lista

--- pilha.py
+++ pilha.py
@@ -1,6 +1,12 @@
 from typing import List
 
 stack: List[str] = []
-stack.append('A')
 
-print(stack)
+stack.append('A')
+stack.append('B')
+stack.append('C')
+
+top_item = stack.pop() # Removendo C
+top_item = stack.pop() # removendo B
+
+print(stack, top_item)
